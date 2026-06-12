485accf360ff94dfd3a79bceb81c832522b35acd
BdSoft Tech <admin@bdsoft.org>
2026-06-12 13:07:09 +0600

    Update appointment pdf editor design error solve

--- app/candidiate/page.tsx
+++ app/candidiate/page.tsx
@@ -132,7 +132,10 @@ export default function ApplicationsPage() {
             <table className="w-full text-sm">
               <thead className="bg-white/10">
                 <tr>
+                  <th className="p-3 text-left">Typr</th>
+
                   <th className="p-3 text-left">Date</th>
+                  
                   <th className="p-3 text-left">Name</th>
                   <th className="p-3 text-left">Number</th>
                   <th className="p-3 text-left">Email</th>
